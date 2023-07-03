bec8d325c65315830aec92a75fb2dae74a10535f
abhi-rout <130905508+abhi-rout@users.noreply.github.com>
2023-07-03 23:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 function getFormvalue() {
 	 
-        // var divElement = document.createElement('div');
-        // divElement.setAttribute('id', 'abc');
-        // document.body.appendChild(divElement);
-	var form = document.getElementById('myForm');
-  
-  // Get the values of the first and last name input fields
-  var firstName = form.firstName.value;
-  var lastName = form.lastName.value;
-  
-  // Display the values using alert
-  alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
+        var divElement = document.createElement('div');
+        divElement.setAttribute('id', 'abc');
+        document.body.appendChild(divElement);
+	
 
 }
